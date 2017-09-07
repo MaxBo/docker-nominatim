67f26866c96d0f2db19ae4b657574aa29a90eff7
Nicolas Ternisien <nicolas.ternisien@gmail.com>
2017-09-07 21:52:08 +0200

    Update Docker file

--- nominatim/local.php
+++ nominatim/local.php
@@ -1,8 +1,8 @@
 <?php
  // General settings
  @define('CONST_Database_DSN', 'pgsql://nominatim@' . getenv('PGHOST') . ':5432/nominatim'); // <driver>://<username>:<password>@<host>:<port>/<database>
- @define('CONST_Website_BaseURL', '/');
+ @define('CONST_Website_BaseURL', '/nominatim/');
  // Software versions
- @define('CONST_Postgresql_Version', '9.4');
- @define('CONST_Postgis_Version', '2.3');
+ @define('CONST_Postgresql_Version', '9.5');
+ @define('CONST_Postgis_Version', '2.2');
 ?>
